8cfb522e9a7358d2443e9d965235f8e55115f7d5
agomunsecreteriat-star <agomunsecreteriat@gmail.com>
2026-08-18 21:46:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("AGORA MUN website loaded.");
+const loginForm = document.getElementById("loginForm");
+const errorMessage = document.getElementById("errorMessage");
+
+loginForm.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value;
+
+    // TEST LOGIN
+    const correctEmail = "chair@agomun.org";
+    const correctPassword = "AGOMUN2026";
+
+    if (email === correctEmail && password === correctPassword) {
+
+        errorMessage.style.display = "none";
+
+        alert("Login successful!");
+
+        window.location.href = "dashboard.html";
+
+    } else {
+
+        errorMessage.textContent =
+            "Invalid email or password.";
+
+        errorMessage.style.display = "block";
+    }
 });
